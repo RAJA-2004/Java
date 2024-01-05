7ca7c1656f35132c4414e24f161659bcfa69ff02
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-01-05 22:04:25 +0530

    added revTriangle.java

---
+++ Pattern/revTriangle.java
@@ -0,0 +1,12 @@
+package Pattern;
+
+public class revTriangle {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=i;j<5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
