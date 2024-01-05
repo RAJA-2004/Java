d6d9f07677ca10c018de12a0837fd954e27a1ac1
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-01-05 22:03:57 +0530

    added triangle.java

---
+++ Pattern/triangle.java
@@ -0,0 +1,12 @@
+package Pattern;
+
+public class triangle {
+    public static void main(String[] args) {
+        for(int i=5;i>0;i--){
+            for(int j=i;j<6;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
