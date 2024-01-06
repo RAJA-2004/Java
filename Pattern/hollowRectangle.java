3359bb326db77bca227e5325393d96c41acfea02
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-01-06 15:31:47 +0530

    added hollowRectangle.java

---
+++ Pattern/hollowRectangle.java
@@ -0,0 +1,19 @@
+package Pattern;
+
+public class hollowRectangle {
+        public static void main(String[] args) {
+        int rows = 6;
+        int columns = 6;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (i == 0 || i == rows - 1 || j == 0 || j == columns - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    } 
+}
