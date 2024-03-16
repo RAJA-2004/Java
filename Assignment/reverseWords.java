5aed50c183d487244bbeb4bfb582c8494ad482f0
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-03-16 18:26:18 +0530

    Create reverseWords.java

---
+++ Assignment/reverseWords.java
@@ -0,0 +1,18 @@
+public class reverseWords {
+    public static void main(String[] args) {
+        String inputString = "Java is Cool";
+        String[] words = inputString.split(" ");
+        StringBuilder reversedString = new StringBuilder();
+
+        for (String word : words) {
+            StringBuilder reversedWord = new StringBuilder(word);
+            reversedWord.reverse();
+            reversedString.append(reversedWord).append(" ");
+        }
+        
+        reversedString.deleteCharAt(reversedString.length() - 1);
+
+        System.out.println("Input String: " + inputString);
+        System.out.println("Output: " + reversedString.toString());
+    }
+}
