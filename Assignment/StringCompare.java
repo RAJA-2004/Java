91d5b48ebfbf7db7d550561a23298340dfdc0bc3
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-03-16 18:26:11 +0530

    Create StringCompare.java

---
+++ Assignment/StringCompare.java
@@ -0,0 +1,12 @@
+public class StringCompare {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "hello";
+
+        if (str1.equals(str2)) {
+            System.out.println("Strings are equal.");
+        } else {
+            System.out.println("Strings are not equal.");
+        }
+    }
+}
