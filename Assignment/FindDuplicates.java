58d51778e4dbf87f0526cdd3c754df2a20e358c7
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-03-16 18:26:28 +0530

    Create FindDuplicates.java

---
+++ Assignment/FindDuplicates.java
@@ -0,0 +1,13 @@
+package Assignment;
+class StringCompare {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "hello";
+
+        if (str1.equals(str2)) {
+            System.out.println("Strings are equal.");
+        } else {
+            System.out.println("Strings are not equal.");
+        }
+    }
+}
