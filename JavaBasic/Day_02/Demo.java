5944b047a353189ac8be29c1a2ca08a31828a2f1
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-03-21 09:32:02 +0530

    Create Demo.java

---
+++ JavaBasic/Day_02/Demo.java
@@ -0,0 +1,11 @@
+// ! package are like folders
+
+package Day_02;
+
+public class Demo {
+    public static void main(String[] args){
+        // ! 30 "raja"
+        System.out.println(args[0]); // 30
+        System.out.println(args[1]); // raja
+    }
+}
