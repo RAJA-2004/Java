d67461c62cc00eb1580ad3a1f22d87c0b6cbf9b9
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-03-23 09:16:57 +0530

    Delete basic.java

--- JavaBasic/basic.java
+++
@@ -1,8 +0,0 @@
-public class basic { // class name and file name should be same
-    public static void main(String arg[]){
-        System.out.println("Hello World");
-        System.out.println("Raja Digvijay Singh");
-        System.out.print("BTech Cse\n");
-    }
-}
-// boiler plate
