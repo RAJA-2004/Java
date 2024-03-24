815e8de4febce5023c584efb6e5c098c7d6bb88b
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-03-24 10:00:09 +0530

    Create primitiveType.java

---
+++ JavaBasic/Day_02/primitiveType.java
@@ -0,0 +1,19 @@
+package Day_02;
+
+import java.util.*;
+
+public class primitiveType {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+
+        int a = input.nextInt();
+        float b = input.nextFloat();
+        long c = input.nextLong();
+        
+        System.out.println(str);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
