3c39e56f61bb8a5623e8b742cdb00964aaeab668
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-03-25 19:55:06 +0530

    Create basic.java

---
+++ JavaBasic/Day_01/basic.java
@@ -0,0 +1,14 @@
+// ! class name and file name should be same
+public class basic { 
+
+    //  ! public -> so that it can be accessable from anywhere
+    //  ! static -> so that we do not have to create any object of the class to run main
+    //  ! void   -> it is return type and we dont want to return any value
+    //  ! String[] args -> command line arguments
+    public static void main(String arg[]){
+        System.out.println("Hello World");
+        System.out.println("Raja Digvij ay Singh");
+        System.out.print("BTech Cse\n");
+    }
+}
+// boiler plate
