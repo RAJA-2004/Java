4f8e75e2cf0cb422b9765c0fdac691f2e98f4727
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-04-02 14:33:57 +0530

    Update basic2.java

--- Exam/basic2.java
+++ Exam/basic2.java
@@ -6,10 +6,35 @@ import java.util.Scanner;
 public class basic2 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-        String str = obj.nextLine();
-        char[] arr = str.toCharArray();
-        Arrays.sort(arr);
-        String newstr = new String(arr);
-        System.out.println(newstr);
+        int n = obj.nextInt();
+        int one = 0;
+        int two = 0;
+        int three = 0;
+        int four = 0;
+        int five = 0;
+        int NOTA = 0;
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = obj.nextInt();
+            if(arr[i]==1){
+                one++;
+            }
+            else if(arr[i]==2){
+                two++;
+            }
+            else if(arr[i]==3){
+                three++;
+            }
+            else if(arr[i]==4){
+                four++;
+            }
+            else if(arr[i]==5){
+                five++;
+            }
+            else{
+                NOTA++;
+            }
+        }
+        System.out.println(one+" "+two+" "+three+" "+four+" "+five+" "+NOTA);
     }
 }
